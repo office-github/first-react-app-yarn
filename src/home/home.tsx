dc7d798fd22cafd0ba8c7729dcdd2727724618f8
Bijay Yadav <bijay.yadav@cloudyfox.com>
2018-11-01 14:55:57 +0545

    First Commit to the react js with the yarn

---
+++ src/home/home.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Home extends React.Component {
+    render() {
+        return(
+            <div className="home">
+                <div>Home Page</div>
+            </div>
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
+import Home from './home/home'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('home'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
